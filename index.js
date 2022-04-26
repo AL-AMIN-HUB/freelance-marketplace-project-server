ab9010bb306e4e1488ed4894a718a3416f279f7f
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-04-26 16:23:59 +0600

    commited

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Acces-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Acces-Contorl-Allow-Methods', 'Content-Type', 'application/json');
   next();
 });
 // const corsOptions = {
