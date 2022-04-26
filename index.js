1c4b0c7425c4632a992ca673e22c95bde10598f9
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-04-26 16:18:13 +0600

    commited

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ const corsOptions = {
   origin: "*"
 }
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // const corsOptions = {
 //   origin: '*',
 //   credentials: true,            //access-control-allow-credentials:true
