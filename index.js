a80cf8f5e6b7d908a910143be04d59aff7c0ce54
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-05-07 21:27:28 +0600

    commited

--- index.js
+++ index.js
@@ -12,44 +12,43 @@ const mailRoute = require("./routes/mailRoute");
 const { append } = require("express/lib/response");
 const port = process.env.PORT || 8000;
 
+// blocking cors errors:
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["*"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 dotenv.config();
 app.set("view engine", "ejs");
 
-  // Database connection with mongoose___
-  mongoose
-    .connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-    .then(() => console.log("Connection successfully!"))
-    .catch((err) => console.log(err));
+// Database connection with mongoose___
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connection successfully!"))
+  .catch((err) => console.log(err));
 
-  // Main page or Home page__
-  app.get("/", (req, res) => {
-    res.render("pages/home");
-  });
+// Main page or Home page__
+app.get("/", (req, res) => {
+  res.render("pages/home");
+});
 
-  // All Routes Here__
-  app.use("/auth/users", authRoute);
-  app.use("/auth/gigs", gigsRoute);
-  app.use("/auth/reviews", reviewRoute);
-  app.use("/auth/task", taskRoute);
-  app.use("/auth/notifictions", notifyRoute);
-  app.use("/auth/mails", mailRoute);
-
-  // blocking cors errors:
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: ["GET", "POST"],
-  allowedHeaders: ["*"],
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+// All Routes Here__
+app.use("/auth/users", authRoute);
+app.use("/auth/gigs", gigsRoute);
+app.use("/auth/reviews", reviewRoute);
+app.use("/auth/task", taskRoute);
+app.use("/auth/notifictions", notifyRoute);
+app.use("/auth/mails", mailRoute);
 
 app.listen(port, (req, res) => {
   console.log(`Server is running in port no ${port}`);
