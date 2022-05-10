0ddb8f578fc2d823d99f9655835330bf01e9080c
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-05-11 05:21:20 +0600

    commited after init jest

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,15 @@
+const request = require("supertest");
+const app = require("../src/index");
 describe("App Test Starting Point", () => {
   test("App should be defined", () => {
     console.log("App is defined");
+  });
+
+  //
+  test("App should be running", async () => {
+    console.log("App is running");
+    let response = await request(app).get("/auth/users");
+    expect(response.statusCode).toBe(200);
+    console.log(response.body);
   });
 });
